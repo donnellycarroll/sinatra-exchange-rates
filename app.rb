b5170bf610a559a4e9d7ad3644e778e4d114975f
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-25 21:24:43 +0000

    started

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
+# define a route
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  # build the API url, including the API key in the query string
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+  
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  pp @symbols = parsed_data.keys
+
+  currency_hash = parsed_data.fetch("currencies")
+
+  currency_hash.each do |code, name|
+    pp "Currency Code: #{code}"
+    pp "Currency Name #{name}"
+  end
+
+  
+
+  # render a view template where I show the symbols
+  # erb(:homepage)
 end
